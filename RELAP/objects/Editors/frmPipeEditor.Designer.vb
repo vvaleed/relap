16e5e264d0eb7aec24c70a5bba0b0b0ef3c8b06e
vvaleed_cp <SND\vvaleed_cp>
2013-07-11 05:11:40 +0000

    pipe DGV added

--- RELAP/objects/Editors/frmPipeEditor.Designer.vb
+++ RELAP/objects/Editors/frmPipeEditor.Designer.vb
@@ -22,7 +22,18 @@ Partial Class frmPipeEditor
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.UcPipeEditor1 = New ucPipeEditor()
+        Me.UcPipeEditor1 = New Global.RELAP.ucPipeEditor()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.VolumeNumber = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Azimuthalangle = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Flowarea = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.HyrdraulicDiameter = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Junctionflowarea = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.LengthofVolume = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.VerticalAngle = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.VolumeofVolume = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.WallRoughness = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'UcPipeEditor1
@@ -35,16 +46,86 @@ Partial Class frmPipeEditor
         Me.UcPipeEditor1.SystemOfUnits = Nothing
         Me.UcPipeEditor1.TabIndex = 0
         '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.VolumeNumber, Me.Azimuthalangle, Me.Flowarea, Me.HyrdraulicDiameter, Me.Junctionflowarea, Me.LengthofVolume, Me.VerticalAngle, Me.VolumeofVolume, Me.WallRoughness})
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 12)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(970, 289)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'VolumeNumber
+        '
+        Me.VolumeNumber.HeaderText = "Volume Number"
+        Me.VolumeNumber.Name = "VolumeNumber"
+        Me.VolumeNumber.ReadOnly = True
+        '
+        'Azimuthalangle
+        '
+        Me.Azimuthalangle.HeaderText = "Azimuthal Angle"
+        Me.Azimuthalangle.Name = "Azimuthalangle"
+        '
+        'Flowarea
+        '
+        Me.Flowarea.HeaderText = "Flow Area"
+        Me.Flowarea.Name = "Flowarea"
+        '
+        'HyrdraulicDiameter
+        '
+        Me.HyrdraulicDiameter.HeaderText = "Hyrdraulic Diameter"
+        Me.HyrdraulicDiameter.Name = "HyrdraulicDiameter"
+        '
+        'Junctionflowarea
+        '
+        Me.Junctionflowarea.HeaderText = "Junction Flow Area"
+        Me.Junctionflowarea.Name = "Junctionflowarea"
+        '
+        'LengthofVolume
+        '
+        Me.LengthofVolume.HeaderText = "Length of Volume"
+        Me.LengthofVolume.Name = "LengthofVolume"
+        '
+        'VerticalAngle
+        '
+        Me.VerticalAngle.HeaderText = "Vertical Angle"
+        Me.VerticalAngle.Name = "VerticalAngle"
+        '
+        'VolumeofVolume
+        '
+        Me.VolumeofVolume.HeaderText = "Volume of Volume"
+        Me.VolumeofVolume.Name = "VolumeofVolume"
+        '
+        'WallRoughness
+        '
+        Me.WallRoughness.HeaderText = "Wall Roughness"
+        Me.WallRoughness.Name = "WallRoughness"
+        '
         'frmPipeEditor
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.ClientSize = New System.Drawing.Size(1009, 327)
+        Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.UcPipeEditor1)
         Me.Name = "frmPipeEditor"
-        Me.Text = "frmPipeEditor"
+        Me.Text = "Pipe Editor"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
     Friend WithEvents UcPipeEditor1 As ucPipeEditor
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents VolumeNumber As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Azimuthalangle As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Flowarea As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents HyrdraulicDiameter As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Junctionflowarea As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents LengthofVolume As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents VerticalAngle As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents VolumeofVolume As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents WallRoughness As System.Windows.Forms.DataGridViewTextBoxColumn
 End Class
--- RELAP/objects/Editors/frmPipeEditor.vb
+++ RELAP/objects/Editors/frmPipeEditor.vb
@@ -1,3 +1,7 @@
 ﻿Public Class frmPipeEditor
 
+    Private Sub frmPipeEditor_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        'DataGridView1.Rows.Add(RELAP.SimulationObjects.UnitOps.pipe
+    End Sub
 End Class
