471cc8fa32da41d31bcfc74215b61e4fc91d3df7
sarahwaleed_cp <SND\sarahwaleed_cp>
2013-09-29 18:50:19 +0000

    Minor Edit Fix

--- RELAP/frmMinorEditRequests.vb
+++ RELAP/frmMinorEditRequests.vb
@@ -1,20 +1,7 @@
 ﻿Public Class frmMinorEditRequests
     Inherits WeifenLuo.WinFormsUI.Docking.DockContent
 
-    Private Sub frmMinorEditRequests_Shown(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Shown
-        Try
-
-
-            cboParameter.Items.Clear()
-
-            For Each kvp As KeyValuePair(Of String, SimulationObjects_BaseClass) In My.Application.ActiveSimulation.Collections.ObjectCollection
-                cboParameter.Items.Add(kvp.Value.GraphicObject.Tag)
-
-            Next
-        Catch ex As Exception
-
-        End Try
-    End Sub
+   
 
     Private Sub DataGridView1_CellEndEdit(ByVal sender As Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles DataGridView1.CellEndEdit
         If e.ColumnIndex = 0 Then
@@ -27,5 +14,14 @@
                 DataGridView1.Columns(1).Visible = True
             End If
         End If
+    End Sub
+
+    Private Sub frmMinorEditRequests_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        cboParameter.DisplayMember = "Tag"
+        Try
+            cboParameter.DataSource = New BindingSource(My.Application.ActiveSimulation.FormSurface.FlowsheetDesignSurface.drawingObjects, Nothing)
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
     End Sub
 End Class
